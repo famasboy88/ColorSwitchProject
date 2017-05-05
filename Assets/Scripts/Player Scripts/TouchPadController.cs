cc223271c18a971b9f6197821843e14966f027e1
famasboy88 <famasboy88@gmail.com>
2017-05-05 15:21:43 +0800

    Fixed some jumping bugs

--- Assets/Scripts/Player Scripts/TouchPadController.cs
+++ Assets/Scripts/Player Scripts/TouchPadController.cs
@@ -18,19 +18,21 @@ public class TouchPadController : MonoBehaviour {
 		lastPos = dynamicObj.transform.position;
 	} 
 		
+	void playerJump(){
+		if (GameController.GetComponent<GameController> ().getIsDead () == false) {
+			player.GetComponent<Rigidbody2D> ().gravityScale = 1f;
+			player.GetComponent<Rigidbody2D> ().velocity = rb2d.velocity = new Vector2 (0f, 0f);
+			if (player.transform.position.y > -1) {
+				rb2d.AddForce (new Vector2 (0, upForce * -1));
+			}
+			if (player.transform.position.y < 4.7f) {
+				player.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, upForce * 1));
+			}
+		}
+	}
 
 	void FixedUpdate(){
 		if(GameController.GetComponent<GameController> ().getIsDead()==false){
-			if(Input.GetMouseButtonDown(0)){
-				player.GetComponent<Rigidbody2D> ().gravityScale = 1f;
-				player.GetComponent<Rigidbody2D> ().velocity = rb2d.velocity = new Vector2(0f,0f);
-				if(player.transform.position.y>-0.63f){
-					rb2d.AddForce (new Vector2(0,upForce*-1));
-				}
-				if(player.transform.position.y<4.7f){
-					player.GetComponent<Rigidbody2D> ().AddForce (new Vector2(0,upForce*1));
-				}
-			}
 			if (dynamicObj.transform.position.y > lastPos.y) {
 				rb2d.gravityScale = 0f;
 				rb2d.velocity = new Vector2(0f,0f);

8b7a8e091bdfb441de289e773983354b4bae7c2c
famasboy88 <famasboy88@gmail.com>
2017-05-09 19:27:42 +0800

    Prototype for back and forth arrow enemy

---
+++ Assets/Scripts/Enemy Scripts/ArrowEnemyControllerLeft.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowEnemyControllerLeft : MonoBehaviour {
+
+	public float speed = 3;
+	public float dir = 1;
+
+	void Update(){
+		transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * dir * speed);
+	}
+
+	//check if the object hits the other two cubes
+	void OnTriggerEnter2D(Collider2D coll){
+		//check if the object enter into a trigger with objects named Finish
+		if(coll.tag=="bounceBack"){
+			//reverse the current direction 
+			dir *= -1;
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy Scripts/ArrowEnemyControllerRight.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowEnemyControllerRight : MonoBehaviour
+{
+
+
+	public float speed = 3;
+	public float dir = -1f;
+
+	void Update(){
+		transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * dir * speed);
+	}
+
+	//check if the object hits the other two cubes
+	void OnTriggerEnter2D(Collider2D coll){
+		//check if the object enter into a trigger with objects named Finish
+		if(coll.tag=="bounceBack"){
+			//reverse the current direction 
+			dir *= -1;
+		}
+	}
+
+
+}
+
